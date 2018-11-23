f240c868bb1cb27f5a3a18471879951121911fee
happou31 <happo31@gmail.com>
2018-11-23 17:42:18 +0900

    test

---
+++ src/renderer/App/ImageView/ImageView.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+  imgUrl: string;
+};
+
+export default class ImageView extends React.Component<Props> {
+  render() {
+    return (
+      <>
+        <div>Hello, world!</div>
+      </>
+    );
+  }
+}
