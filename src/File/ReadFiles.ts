79ad3f93a3f2cb29f9825782002216807fbaa6fb
happou31 <happo31@gmail.com>
2018-11-26 22:22:00 +0900

    add: filter

--- src/File/ReadFiles.ts
+++ src/File/ReadFiles.ts
@@ -1,11 +1,13 @@
 import fs from "fs";
 
-export default async function ReadFiles(dirPath: string) {
+export default async function ReadFiles(dirPath: string, filterTest?: RegExp) {
   return new Promise<string[]>((resolve, reject) => {
     fs.readdir(dirPath, (err, files) => {
       if (err) {
         reject(err);
         return;
+      } else if (filterTest) {
+        resolve(files.filter(files => filterTest.test(files)));
       } else {
         resolve(files);
       }
