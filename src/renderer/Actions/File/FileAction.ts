9e7fa0e1393a5d3db31229cea6d89199ab27eb0f
happou31 <happo31@gmail.com>
2018-11-26 21:09:19 +0900

    rename

--- src/renderer/Actions/File/FileAction.ts
+++ src/renderer/Actions/File/FileAction.ts
@@ -1,7 +1,9 @@
 import { createAppAction } from "../ActionType";
+import { OpenDialogOptions } from "electron";
 
 export enum ActionType {
   FOLDERCHANGE = "FOLDERCHANGE",
+  OPENDIALOG = "OPENDIALOG",
   INDEX_INCREMENT = "INDEX_INCREMENT",
   INDEX_DECREMENT = "INDEX_DECREMENT"
 }
@@ -13,16 +15,28 @@ const folderChange = createAppAction(
   })
 );
 
+const openDialog = createAppAction(
+  ActionType.OPENDIALOG,
+  (option: OpenDialogOptions, callback: (filePaths: string) => void) => ({
+    ...option,
+    callback
+  })
+);
+
 const indexIncrement = createAppAction(ActionType.INDEX_INCREMENT);
 
 const indexDecrement = createAppAction(ActionType.INDEX_DECREMENT);
 
 export type FileAction = ReturnType<
-  typeof folderChange | typeof indexIncrement | typeof indexDecrement
+  | typeof folderChange
+  | typeof openDialog
+  | typeof indexIncrement
+  | typeof indexDecrement
 >;
 
 export const fileActions = {
   folderChange,
+  openDialog,
   indexIncrement,
   indexDecrement
 };
