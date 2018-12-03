a60dcfd149e515d6f7cdd2fc10f30270049b1dd6
happou31 <happo31@gmail.com>
2018-12-04 00:07:18 +0900

    wip: 無意味化してしまった

--- src/renderer/View/Header/Header.tsx
+++ src/renderer/View/Header/Header.tsx
@@ -7,31 +7,15 @@ type Props = {
 };
 
 const AppRegionArea = styled.div`
-  text-align: center;
-  height: 30px;
-  flex: 1;
-  -webkit-app-region: drag;
-`;
-
-const VirticalRight = styled.div`
-  text-align: right;
-  width: 25px;
+  height: 100%;
 `;
 
 export default class Header extends React.Component<Props> {
   render() {
     return (
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <AppRegionArea>{this.props.title}</AppRegionArea>
-        <VirticalRight>
-          <button
-            style={{ display: "block", width: "100%" }}
-            onClick={this.props.onClickClose}
-          >
-            ×
-          </button>
-        </VirticalRight>
-      </div>
+      <>
+        <AppRegionArea />
+      </>
     );
   }
 }
